5715e4f0be2cd39133d6b28451452c6cbd66a1e5
Agnieszka Dragaczyk <Agnieszka.127@interia.pl>
2021-04-30 13:14:44 +0200

    add function for id result2

--- JS/app.js
+++ JS/app.js
@@ -22,5 +22,34 @@
     }
     hello();
  
+// div id return2
+    let result2 = document.getElementById("result2");
 
- 
+    function add2(c,d)
+    {
+        return "Wynik = " + (c + d) //zwracanie funkcji jako równanie do nowej zmiennej
+                    // return funkja kończy swoje wykonywanie
+        alert("next") ///już się nie wyświetli return - funkcja skoku kończy funkcję i wychodzi z danej funkcji
+    }
+
+    let sum = add2(5,5); //efekt zwrócenia funkcji do nowej zmiennej
+    result2.innerHTML += "Suma = " + sum + "</br>";
+    // alert(add2(10,30))
+
+    // rekurencja // funkcja rekurencyjna wywołuje wewnątrz swego ciała samą siebie
+
+    //silnia n = 5, 5 * 4 * 3 * 2 * 1 
+
+    // function silnia(n)
+    // {
+       
+    //     // silnia(n-1); //wywolywanie wewnątrz funkcji samej funkcji - tworzy się pętla nieskończona
+    //     // dla silni licz mniejszych niż 0 nie obliczamy
+    //     // silnia to n * wszystkie jej mniejsze składowe
+    //     if (n == 1);
+    //         return 1;
+    //     else 
+    //         return n * silnia (n-1);
+    // }
+
+    // result.innerHTML += "Silnia = " + silnia(5);
